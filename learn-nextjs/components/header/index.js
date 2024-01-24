7cb00ae6b223f541494256c8bccbbb9410dc34ae
Billy Hasman <billyhasman08@gmail.com>
2024-01-24 08:32:40 +0700

    Updated_Header fix tailwind list biar mirip sama contoh

--- learn-nextjs/components/header/index.js
+++ learn-nextjs/components/header/index.js
@@ -13,17 +13,19 @@ import styles from './styles.module.css'
 export default function Header({ number, handleSetNumber }) {
   return (
     <div className={styles.header}>
-      <ul className={'list-disc list-inside'}>
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-        <li>
-          <Link href='/profile'>Profile</Link>
-        </li>
-        <li>
-          <Link href='/users'>Users</Link>
-        </li>
-      </ul>
+      <div>
+        <ul className='list-disc px-5 list-inside'>
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+          <li>
+            <Link href='/profile'>Profile</Link>
+          </li>
+          <li>
+            <Link href='/users'>Users</Link>
+          </li>
+        </ul>
+      </div>
 
       {/* <div>
         <button onClick={handleSetNumber}>Button Number</button>
